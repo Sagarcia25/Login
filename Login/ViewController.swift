90bb80ab1bb1e5db27b538b95ff3ebb6d4d6292c
Salvador <Sagarcia2713@gmail.com>
2021-09-14 22:18:12 -0500

    Implemented logic in the prepare function to determine what the title of the landing view controller should be. A guard let is used to check if the sender was from a UIButton. If so, check what UIButton was pressed.

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -26,14 +26,17 @@ class ViewController: UIViewController {
         performSegue(withIdentifier: "Forgot Username/Password", sender: sender)
     }
     
-    
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = usernameTextField.text
+        guard let sender = sender as? UIButton else { return }
+        
+        if sender == forgotPasswordButton{
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else if sender == forgotUsernameButton{
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title = usernameTextField.text
+        }
     }
     
-    
-
-
 }
 
