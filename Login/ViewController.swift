0e6fac6a6d9058341d6ed854c09e8d4347dbae32
Salvador <Sagarcia2713@gmail.com>
2021-09-14 21:47:36 -0500

    Implemented a segue the will push to the landing screen when the log in button is pressed. Made the navigation controller prefer large titles. Added an IBOutlet to reference the text property of the textfield. Added a prepare function that will will pass the text inside the username textfield and set it as the title for the destination view controller's navigation item title.

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var usernameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = usernameTextField.text
     }
 
 
