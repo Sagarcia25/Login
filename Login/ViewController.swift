5a5a0352a36f473c904cd66a156e380b0f4a5a68
Salvador <Sagarcia2713@gmail.com>
2021-09-14 22:00:53 -0500

    Added IBOutlets to the other two remaining buttons for forgot password and forgot username. Also created IBactions for those two buttons and added the performSegue methods within both IBActions. Created another show segue from the view controller to the destination view controller and gave it an identifier "Forgot Username/Password"

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var forgotUsernameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func forgotUsernameButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "Forgot Username/Password", sender: sender)
+    }
+    
+    @IBAction func forgotPasswordButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "Forgot Username/Password", sender: sender)
+    }
+    
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         segue.destination.navigationItem.title = usernameTextField.text
     }
+    
+    
 
 
 }
